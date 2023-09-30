120e648ca42d2663f1da1309a0c99e2e6b7ccb46
Benjamin Kindle <benjaminkindle@yahoo.com>
2023-09-30 13:58:58 -0400

    feat: show participant count

--- apps/open-standup-solid/vite.config.ts
+++ apps/open-standup-solid/vite.config.ts
@@ -1,10 +1,25 @@
 import vercel from "solid-start-vercel";
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import solidSvg, { SolidSVGPluginOptions } from "vite-plugin-solid-svg";
+
+const svgoConfig: SolidSVGPluginOptions = {
+  svgo: {
+    enabled: true,
+    svgoConfig: {
+      plugins: [
+        "preset-default",
+        {
+          name: "addClassesToSVGElement",
+          params: {
+            classNames: ["w-6", "h-6", "fill-base-content"],
+          },
+        },
+      ],
+    },
+  },
+};
 
 export default defineConfig({
-  envPrefix: "VITE_",
-  plugins: [solid({ adapter: vercel({ edge: false }) })],
+  plugins: [solid({ adapter: vercel({ edge: false }) }), solidSvg(svgoConfig)],
 });
-
-// change
