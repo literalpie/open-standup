c553dbe8589ab05d89aaaace94ef5c65258b47c4
Benjamin Kindle <benjaminkindle@yahoo.com>
2024-09-21 11:43:12 -0400

    Update deps (#31)

--- apps/open-standup-solid/tailwind.config.cjs
+++ apps/open-standup-solid/tailwind.config.cjs
@@ -31,5 +31,6 @@ export default {
       },
     },
   },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
